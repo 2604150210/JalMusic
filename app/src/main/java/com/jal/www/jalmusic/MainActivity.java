82c9adadd9799eec294e43d9f28d204af6f450c6
chenbo <chenbo@agenewtech.com>
2019-03-26 10:08:31 +0800

    mp3播放列表

--- app/src/main/java/com/jal/www/jalmusic/MainActivity.java
+++ app/src/main/java/com/jal/www/jalmusic/MainActivity.java
@@ -1,13 +1,32 @@
 package com.jal.www.jalmusic;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.ListView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-
+    private ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        listView = (ListView) this.findViewById(R.id.listView1);
+        List<Music> listMusic = MusicList.getMusicData(getApplicationContext());
+        for (Music music : listMusic){
+            System.out.println(music);
+        }
+        MusicAdapter adapter = new MusicAdapter(this, listMusic);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
