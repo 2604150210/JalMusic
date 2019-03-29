d5ef4df350fd2a63cb3686b9f39015867f1660f3
chenbo <chenbo@agenewtech.com>
2019-03-29 16:31:27 +0800

    动态获取权限

--- app/src/main/java/com/jal/www/jalmusic/MainActivity.java
+++ app/src/main/java/com/jal/www/jalmusic/MainActivity.java
@@ -1,16 +1,25 @@
 package com.jal.www.jalmusic;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ListView listView;
@@ -18,13 +27,20 @@ public class MainActivity extends AppCompatActivity {
     private Context mContext;
     private TextView tv_main_title;
     private ArrayList<Music> listMusic;
+    private String TAG = "MainActivityLog";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        requestPermission();
+     }
+
+    private void initView() {
         mContext = getApplicationContext();
         listView = this.findViewById(R.id.listView1);
         listMusic = MusicList.getMusicData(getApplicationContext());
+        Log.i(TAG, "listMusic.size()=="+listMusic.size());
         MusicAdapter adapter = new MusicAdapter(this, listMusic);
         listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -40,27 +56,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         cur_music = findViewById(R.id.cur_music);
-        if (MusicService.lastPlayer != null && MusicService.lastMusic != null){
-            cur_music.setVisibility(View.VISIBLE);
-            tv_main_title = findViewById(R.id.tv_main_title);
-            tv_main_title.setText(MusicService.lastMusic.getName());
-            cur_music.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Bundle bundle = new Bundle();
-                    bundle.putParcelableArrayList("listMusic",listMusic);
-                    int position = listMusic.indexOf(listMusic);
-                    bundle.putInt("position", position);
-                    Intent intent = new Intent();
-                    intent.putExtras(bundle);
-                    intent.setClass(MainActivity.this, DetailsActivity.class);
-                    startActivity(intent);
-                }
-            });
-        }else{
-            cur_music.setVisibility(View.GONE);
-        }
-
     }
 
     @Override
@@ -87,5 +82,46 @@ public class MainActivity extends AppCompatActivity {
             cur_music.setVisibility(View.GONE);
         }
         super.onResume();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0){
+                    for (int i = 0; i < grantResults.length; i++) {
+
+                        int grantResult = grantResults[i];
+                        if (grantResult == PackageManager.PERMISSION_DENIED){
+                            String s = permissions[i];
+                            Toast.makeText(this,s+"权限被拒绝了",Toast.LENGTH_SHORT).show();
+                        }else{
+                            initView();
+                        }
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+    private void requestPermission(){
+
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.READ_EXTERNAL_STORAGE);
+        }
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        }
+
+        if (!permissionList.isEmpty()){
+            ActivityCompat.requestPermissions(this,permissionList.toArray(new String[permissionList.size()]),1);
+        }else {
+            initView();
+        }
     }
 }
